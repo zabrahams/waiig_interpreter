a19da2c7c9917adebe102904972d08157b3ff747
Zach Abrahams <zachabrahams@gmail.com>
2017-02-25 22:15:21 -0700

    Working version of lexer for subset of the language

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,7 +1,10 @@
 // lexer/lexer.go
 package lexer
 
-import "github.com/zachabrahams/waiig_interpreter/token"
+import (
+    "github.com/zachabrahams/waiig_interpreter/token"
+    "fmt"
+)
 
 type Lexer struct {
     input        string
@@ -18,6 +21,8 @@ func New(input string) *Lexer {
 
 func (l *Lexer) NextToken() token.Token {
     var tok token.Token
+
+    l.skipWhitespace()
 
     switch l.ch {
     case '=':
@@ -39,14 +44,39 @@ func (l *Lexer) NextToken() token.Token {
     case 0:
         tok.Literal = ""
         tok.Type = token.EOF
+    default:
+        if isLetter(l.ch) {
+            tok.Literal = l.readIdentifier()
+            fmt.Println(tok.Literal)
+            tok.Type = token.LookupIdent(tok.Literal)
+            return tok
+        } else if isDigit(l.ch) {
+            tok.Type = token.INT
+            tok.Literal = l.readNumber()
+            return tok
+        } else {
+            tok = newToken(token.ILLEGAL, l.ch)
+        }
     }
 
     l.readChar()
     return tok
 }
 
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-    return token.Token{Type: tokenType, Literal: string(ch)}
+func (l *Lexer) readIdentifier() string {
+    position := l.position
+    for isLetter(l.ch) {
+        l.readChar()
+    }
+    return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+    position := l.position
+    for isDigit(l.ch) {
+        l.readChar()
+    }
+    return l.input[position:l.position]
 }
 
 func (l *Lexer) readChar() {
@@ -57,4 +87,22 @@ func (l *Lexer) readChar() {
     }
     l.position = l.readPosition
     l.readPosition += 1
+}
+
+func (l *Lexer) skipWhitespace() {
+    for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+        l.readChar()
+    }
+}
+
+func isLetter(ch byte) bool {
+  return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+  return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+    return token.Token{Type: tokenType, Literal: string(ch)}
 }
