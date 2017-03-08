cbed838a1ef6b92e2b6eb8e1e426737c4c1d25e7
Zach Abrahams <zachabrahams@gmail.com>
2017-03-07 21:19:15 -0700

    Gets parsing of let and return statements working.

---
+++ main.go
@@ -0,0 +1,21 @@
+// main.go
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/user"
+    "github.com/zachabrahams/waiig_interpreter/repl"
+)
+
+func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Hello %s! This is the Monkey Programming Langauge!\n", user.Username)
+    fmt.Printf("Feel free to type in commands\n")
+    repl.Start(os.Stdin, os.Stdout)
+}
